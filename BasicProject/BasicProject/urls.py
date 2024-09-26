8cf43644382484fcec04d504b185622df067f53b
Duncan <84990667+gaidheal1@users.noreply.github.com>
2024-09-27 00:35:38 +0100

    Create new app, update views

--- BasicProject/BasicProject/urls.py
+++ BasicProject/BasicProject/urls.py
@@ -3,14 +3,17 @@ Definition of urls for BasicProject.
 """
 
 from datetime import datetime
-from django.urls import path
+from django.urls import path, include, re_path
 from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
 from app import forms, views
+import HelloDjangoApp.views
 
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    re_path(r'^$', HelloDjangoApp.views.index, name="index"),
+    re_path(r'^home$', HelloDjangoApp.views.index, name="home"),
+    #path('', views.home, name='home'),
     path('contact/', views.contact, name='contact'),
     path('about/', views.about, name='about'),
     path('login/',
